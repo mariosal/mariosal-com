a19891a84aebbc8f74561196bcf2d1fa7ce2383c
Mario Saldinger <mariosaldinger@gmail.com>
2014-08-07 15:24:28 +0300

    Testing.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-  response.send('ok');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
 }).listen(process.env.PORT || 5000);
