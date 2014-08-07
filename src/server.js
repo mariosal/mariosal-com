e73be007d60b449775014a835c7a92baddbee61a
Mario Saldinger <mariosaldinger@gmail.com>
2014-08-07 15:20:05 +0300

    Testing Heroku.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(307, {
-    'Location': 'https://careers.stackoverflow.com/mariosal'
-  });
-  response.end();
+  response.send('ok');
 }).listen(process.env.PORT || 5000);
